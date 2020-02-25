f5a4bb9193bf92cc647f3f31d4e19c428b4eab6c
james <1204788939@qq.com>
2020-02-25 20:52:25 +0800

    配置路由组件懒加载，提升受屏渲染时间

---
+++ node-server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const compression = require('compression');
+
+const port = process.env.PORT || 8090;
+const app = express();
+
+// 开启 gzip 压缩
+app.use(compression);
+// 配置静态文件服务器
+app.use(express.static('./build'));
+
+module.exports = app.listen(port, (err, data) => {
+    if (err) {
+        console.log(err)
+        return;
+    }
+    console.log(`Your node server is listening at http://localhost:${port}\n`);
+})
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Redirect } from 'react-router-dom';
 import Home from '../application/Home';
-import Rank from '../application/Rank';
-import Recommend from '../application/Recommend';
-import Singers from '../application/Singers';
-import Singer from '../application/Singer';
-import Album from '../application/Album';
-import Search from '../application/Search';
+// import Rank from '../application/Rank';
+// import Recommend from '../application/Recommend';
+// import Singers from '../application/Singers';
+// import Singer from '../application/Singer';
+// import Album from '../application/Album';
+// import Search from '../application/Search';
+const SuspenseComponent = Component => props => {
+    return (
+        <Suspense>
+            <Component {...props} />
+        </Suspense>
+    )
+}
+// 配置组件懒加载，可以进行代码分割。否则一开始就会加载所有组件，这样有利于提升受屏渲染速度
+const RankComponent = lazy(() => import('../application/Rank'));
+const RecommendComponent = lazy(() => import('../application/Recommend'));
+const SingersComponent = lazy(() => import('../application/Singers'));
+const SingerComponent = lazy(() => import('../application/Singer'));
+const AlbumComponent = lazy(() => import('../application/Album'));
+const SearchComponent = lazy(() => import('../application/Search'));
 
 const Routes = [{
     path: '/',
@@ -20,36 +34,36 @@ const Routes = [{
         component: Home
     }, {
         path: '/rank',
-        component: Rank,
+        component: SuspenseComponent(RankComponent),
         routes: [{
             path: '/rank/:id',
-            component: Album
+            component: SuspenseComponent(AlbumComponent)
         }]
     }, {
         path: '/recommend',
-        component: Recommend,
+        component: SuspenseComponent(RecommendComponent),
         routes: [{
             path: '/recommend/:id',
-            component: Album
+            component: SuspenseComponent(AlbumComponent)
         }]
     }, {
         path: '/singers',
-        component: Singers,
+        component: SuspenseComponent(SingersComponent),
         routes: [{
             path: '/singers/:id',
-            component: Singer
+            component: SuspenseComponent(SingerComponent)
         }]
     }, {
         // 用来显示歌单
         path: '/album:id',
         key: 'album',
         exact: true,
-        component: Album
+        component: SuspenseComponent(AlbumComponent)
     }, {
         path: '/search',
         key: 'search',
         exact: true,
-        component: Search
+        component: SuspenseComponent(SearchComponent)
     }]
 }];
 export default Routes;
